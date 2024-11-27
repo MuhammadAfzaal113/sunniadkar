d26f9bd1ee87ad88abdf9f5d425fb5fc7183edd4
afzaal <m.afzaal@sparkai.ca>
2024-11-27 23:22:42 +0500

    ✨ feat(settings): Add STATIC_ROOT configuration option in settings file

--- sunniadkar/settings.py
+++ sunniadkar/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 AUTH_USER_MODEL = 'user.User'
 
 # Default primary key field type
