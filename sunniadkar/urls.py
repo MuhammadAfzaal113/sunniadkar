41a5b3135d6fe520cee70310db6455a7f0f59d3d
afzaal <m.afzaal@sparkai.ca>
2024-12-12 03:13:48 +0500

    feat(urls.py): Add redirect from root to admin page

--- sunniadkar/urls.py
+++ sunniadkar/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
     path('api/v1/user/', include('apps.user.urls')),
     path('api/v1/', include('apps.modules.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
